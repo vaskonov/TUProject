e226bc5ca7070215c121b9356ae65f81040f0eab
vasily <elgesto@gmail.com>
2014-11-01 18:58:57 +0200

    New version of eval

--- eval.js
+++ eval.js
@@ -5,24 +5,34 @@ var fs = require('fs');
 var filename = process.argv[2]
 var data = JSON.parse(fs.readFileSync(filename));
 
-var stats = new PrecisionRecall();
+var stats_single = new PrecisionRecall();
+var stats_majority = new PrecisionRecall();
 
 _.each(data, function(value, key, list){ 
+	
 	if (!(_.isArray(value['output'])))
 		value['output'] = JSON.parse(value['output'])
 	
-	if (!(_.isArray(value['tags'])))
-		value['tags'] = JSON.parse(value['tags'])
+	if (!(_.isArray(value['tags_single'])))
+		value['tags_single'] = JSON.parse(value['tags_single'])
+
+	if (!(_.isArray(value['tags_majority'])))
+		value['tags_majority'] = JSON.parse(value['tags_majority'])
 
 
 	value['output'] = _.map(value['output'], function(json){ return _.isString(json)? json: JSON.stringify(json) });
-	value['tags'] = _.map(value['tags'], function(json){ return _.isString(json)? json: JSON.stringify(json) });
+	value['tags_single'] = _.map(value['tags_single'], function(json){ return _.isString(json)? json: JSON.stringify(json) });
+	value['tags_majority'] = _.map(value['tags_majority'], function(json){ return _.isString(json)? json: JSON.stringify(json) });
 
-	
-	stats.addCasesHash(value['output'], value['tags'])
-	stats.addCasesLabels(value['output'], value['tags'])
+	stats_single.addCasesHash(value['output'], value['tags_single'])
+	stats_majority.addCasesHash(value['output'], value['tags_majority'])
 
 }, this)
 
-console.log(stats.retrieveStats())
-console.log(stats.retrieveLabels())
+console.log("TAGS SINGLE")
+console.log(stats_single.retrieveStats())
+
+console.log("TAGS MAJORITY")
+console.log(stats_majority.retrieveStats())
+
+
